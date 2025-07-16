9e9cc12250a4ee0e08db9c113e5b0a680c75a4e8
Lucas_397 <lu.machado.m397@gmail.com>
2025-07-16 13:45:10 -0300

    feat/initial-page

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,54 @@
-import { Link } from "expo-router";
-import { Pressable, View } from "react-native";
-import { styles } from "./pomodoro";
+import { Image, View, Text, StyleSheet } from "react-native";
+import { ActionButton } from "@/Components/actionButton";
+import { router } from "expo-router";
 
-export default function index(){
+export default function Index(){
     return(
-        <View>
-            <Pressable style={styles.actionButton}>
-                <Link href={{pathname:'/pomodoro'}} style={styles.actionButtonText}>
-                    Quero iniciar!
-                </Link>
-            </Pressable>
+        <View style={styles.container}>
+            <Image 
+                style={styles.logo} 
+                source={require('../assets/Images/Fokus -  logo-02 1.png')}
+            />
+            <View>
+                <Text
+                style={styles.title}>
+                    Otimize sua produtividade,{'\n'}
+                    <Text style={styles.titleEmphasis}>mergulhe no que importa</Text>
+                </Text>
+            </View>
+
+            <Image source={require('../assets/Images/Imagem tela inicial.png')}/>
+
+            <ActionButton 
+                text={"Quero iniciar!"} 
+                onPress={()=> router.replace('/pomodoro')}
+            />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        backgroundColor: '#021123',
+        alignItems:'center',
+        gap:'45px',
+    },
+    logo:{
+        marginTop:50
+    },
+    title:{
+        maxWidth:300,
+        color:'#FFF',
+        fontSize:26,
+        textAlign:'center',
+    },
+    titleEmphasis:{
+        fontWeight:'bold',
+    },
+    image:{
+    width: '100%',
+    height: '100%',
+    boxSizing:'border-box',
+    },
+})
